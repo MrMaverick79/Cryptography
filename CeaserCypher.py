e47796a8a506bece7c95f22b9e1e94d471c03bca
MrMaverick79 <76990992+MrMaverick79@users.noreply.github.com>
2021-05-15 11:36:55 +1000

    Create CaeserCypher

---
+++ CeaserCypher.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 15th May 2021
+
+Caesar Cypher:
+    Inputs:
+        message - string to be encoded
+        key - string;  the ketter used as the key to the code
+    Returns:
+        encoded message using key 
+        
+
+@author: brendantuckerman
+"""
+
+def CaesarCypher(message, key):
+    alpha =  'abcdefghijklmnopqrstuvwxyz'
+    code = ''
+    lock = alpha.index(key)
+    encodedAlpha = ''
+    encodedAlpha += alpha[lock:] + alpha[:lock] #creates encoded alpha
+    message = message.lower() #turns to lowercase
+    
+    for i in message:
+        if i == ' ':
+            code += ' '
+        else:
+            j = alpha.index(i)
+            code += encodedAlpha[j]
+    return code
+   
+    
+
+    
+
+    
